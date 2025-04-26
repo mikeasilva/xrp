5d70f0e8616e2bbf9657e83f81554e2a26a3c2a2
Mike Silva <mike.a.silva@gmail.com>
2025-04-26 15:03:01 -0400

    Extended the get_distance to return result in lots of units

--- subsystems/sensors.py
+++ subsystems/sensors.py
@@ -10,10 +10,25 @@ class DistanceSensor(commands2.Subsystem):
         """Initialize the distance sensor."""
         self.distance_sensor = xrp.XRPRangefinder()
 
-    def get_distance(self) -> float:
-        """Get the distance from the sensor."""
-        #return wpimath.units.metersToInches(self.distance_sensor.getDistance())
-        return self.distance_sensor.getDistance() * 39.3701 # Return inches
+    def get_distance(self, unit="inch") -> float:
+        """Get the distance from the sensor in various units (inches by default)."""
+        # The distance from the sensor is in meters by default.
+        # Convert to the requested unit.
+        distance = self.distance_sensor.getDistance()
+        if unit == "inch" or unit == "in":
+            return distance * 39.3701
+        elif unit == "cm":
+            return distance * 100
+        elif unit == "meter":
+            return distance
+        elif unit == "feet" or unit == "ft":
+            return distance * 3.28084
+        elif unit == "yard" or unit == "yd":
+            return distance * 1.09361
+        else:
+            raise ValueError(
+                "Invalid unit. Use 'inch', 'feet', 'yard', 'cm', or 'meter'."
+            )
 
 
 class LineSensor(commands2.Subsystem):
