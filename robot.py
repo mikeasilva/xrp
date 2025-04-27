a9138f17425a3f061cf31488ca70c48308a3dd2d
Mike Silva <mike.a.silva@gmail.com>
2025-04-26 20:28:29 -0400

    Added in controller joystick calibration test

--- robot.py
+++ robot.py
@@ -19,6 +19,7 @@ from subsystems.networktables import NetworkTables
 from subsystems.sensors import DistanceSensor, LineSensor
 import time
 import wpilib
+import json
 
 os.environ["HALSIMXRP_HOST"] = "192.168.42.1"
 os.environ["HALSIMXRP_PORT"] = "3540"
@@ -51,6 +52,16 @@ class MyXRP(wpilib.TimedRobot):
 
         with open("log.txt", "w") as f:
             f.write("LOG STARTED\n")
+
+        # Read in the joystick drift
+        try:
+            with open("joystick_drift.json", "r") as f:
+                self.joystick_drift = json.load(f)
+        except:
+            self.joystick_drift = {
+                "left": 0.0,
+                "right": 0.0,
+            }
 
     def _get_current_state(self) -> None:
         """
@@ -142,14 +153,14 @@ class MyXRP(wpilib.TimedRobot):
         # and the user is trying to move the robot foward
         if (
             distance_to_nearest_object <= constants.CRASH_AVOIDANCE_DISTANCE
-            and self.controller.getLeftY() < 0
+            and self._get_left_stick_y() < 0
         ):
             # We need to stop this wreckless behavior
             self.drivetrain.stop()
         else:
             # Drive the robot using the controller
             self.drivetrain.drive(
-                self.controller.getLeftY(), self.controller.getRightX()
+                self._get_left_stick_y(), self._get_right_stick_x()
             )
 
         # Display information about the robot on the console
@@ -186,11 +197,72 @@ class MyXRP(wpilib.TimedRobot):
         self.led.off()
         self.drivetrain.stop()
         with open("log.txt", "a") as f:
-            f.write("test")
+            f.write("test\n")
+        self.n_test = 0
+        self.test_results = {}
+        self.test = "Left Up Test"
 
     def testPeriodic(self) -> None:
-        pass
-        
+        if self.n_test == 41:
+                with open("joystick_calibration.json", "w") as f:
+                    json.dump(self.test_results, f, indent=4)
+                print("Joystick calibration data saved to joystick_calibration.json")
+
+                drift = {}
+                x = 0
+                i = 0
+                for key in ["left_stick_up", "left_stick_down"]:
+                    for item in self.test_results[key]:
+                        x += item
+                        i += 1
+
+                drift["left"] = -(x / i)
+
+                x = 0
+                i = 0
+                for key in ["right_stick_right", "right_stick_left"]:
+                    for item in self.test_results[key]:
+                        x += item
+                        i += 1
+
+                drift["right"] = -(x / i)
+
+                with open("joystick_drift.json", "w") as f:
+                    json.dump(drift, f, indent=4)
+
+
+                self.n_test += 1
+        elif self.n_test == 42:
+            pass
+        else:    
+            print(self.test + ":" + str(self.n_test))
+            if self.controller.getAButtonPressed():
+                left = self.controller.getLeftY()
+                right = self.controller.getRightX()
+                if self.n_test < 11:
+                    result = left
+                    key = "left_stick_up"
+                elif self.n_test < 21:
+                    result = left
+                    key = "left_stick_down"
+                elif self.n_test < 31:
+                    result = right
+                    key = "right_stick_right"
+                elif self.n_test < 41:
+                    result = right
+                    key = "right_stick_left"
+
+                results = self.test_results.get(key, [])
+                results.append(result)
+                self.test_results[key] = results
+                self.n_test += 1
+
+                if self.n_test == 10:
+                    self.test = "Left Down Test"
+                elif self.n_test == 20:
+                    self.test = "Right Right Test"
+                elif self.n_test == 30:
+                    self.test = "Right Left Test"
 
     """
     ====================================================================
@@ -203,3 +275,15 @@ class MyXRP(wpilib.TimedRobot):
 
     def simulationPeriodic(self) -> None:
         pass
+
+    def _get_left_stick_y(self) -> float:
+        """
+        Get the left stick Y value from the controller.
+        """
+        return self.controller.getLeftY() + self.joystick_drift["left"]
+    
+    def _get_right_stick_x(self) -> float:
+        """
+        Get the right stick X value from the controller.
+        """
+        return self.controller.getRightX() + self.joystick_drift["right"]
