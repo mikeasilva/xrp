32f24f2e79142e3bb7278655d0e4ba7c5bc11ebd
Mike Silva <mike.a.silva@gmail.com>
2025-11-27 14:52:27 -0500

    Formatted files

--- state_machines/line_following_routine.py
+++ state_machines/line_following_routine.py
@@ -1,18 +1,21 @@
 import components
-import  magicbot.state_machine
+import magicbot.state_machine
+
 
 class LineFollowingRoutine(magicbot.state_machine.StateMachine):
     drivetrain: components.DriveTrain
     reflectance_sensor: components.ReflectanceSensor
 
     TARGET_BRIGHTNESS = 0.5  # Desired brightness level for line following
-    BASE_SPEED = 0.4         # Base speed of the robot
-    P = 0.6                 # Proportional gain for correction
+    BASE_SPEED = 0.4  # Base speed of the robot
+    P = 0.6  # Proportional gain for correction
 
     @magicbot.state_machine.state(first=True)
     def follow_line(self):
         left_error = self.TARGET_BRIGHTNESS - self.reflectance_sensor.left_reflectance()
-        right_error = self.TARGET_BRIGHTNESS - self.reflectance_sensor.right_reflectance()
+        right_error = (
+            self.TARGET_BRIGHTNESS - self.reflectance_sensor.right_reflectance()
+        )
 
         # Clamp speeds to valid range [-1, 1]
         left_speed = max(min((self.BASE_SPEED + (self.P * left_error)), 1), -1)
@@ -24,6 +27,6 @@ class LineFollowingRoutine(magicbot.state_machine.StateMachine):
     def stop(self):
         self.drivetrain.stop()
 
-    @magicbot.state_machine.timed_state(duration=10, next_state='stop')
+    @magicbot.state_machine.timed_state(duration=10, next_state="stop")
     def timed_follow(self):
         self.follow_line()
